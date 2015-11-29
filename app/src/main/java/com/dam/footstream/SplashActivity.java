502db24b825fabd305660ca9d33e79e03af9faed
Xerahort <xerahort@gmail.com>
2015-11-29 13:32:36 +0100

    Splash Animation

--- app/src/main/java/com/dam/footstream/SplashActivity.java
+++ app/src/main/java/com/dam/footstream/SplashActivity.java
@@ -7,6 +7,11 @@ import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
@@ -18,6 +23,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+
+        TextView name = (TextView)findViewById(R.id.splashname);
+        ImageView logo = (ImageView)findViewById(R.id.splashlogo);
+
+        // load the animation
+        Animation animt = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_move);
+        Animation animi = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_move);
+        animi.setStartOffset(500);
+        name.startAnimation(animt);
+        logo.startAnimation(animi);
 
         Runnable r = new Runnable(){
             public void run(){
